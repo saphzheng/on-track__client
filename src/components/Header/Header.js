aa8ee014c03361bebd0c436ac5d4ebd66c61877e
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-17 16:13:04 -0400

    Set up boilerplate for header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import 'Header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="header__logo" alt="onTrack"></img>
+            <h1 className="header__title">onTrack</h1>
+        </header>
+    );
+}
+
+export default Header;
