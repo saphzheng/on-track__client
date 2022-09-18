cc5e6f043fa8eda7fd082152e6eabd152bae40d9
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-18 18:43:13 -0400

    Set up placeholders for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import './Header.scss';
+import logo from "../../assets/images/ontrack-logo.jpg";
+import { useState } from 'react';
 
 const Header = () => {
+    const [ loggedIn, setLoggedIn ] = useState(false);
+
     return (
         <header className="header">
-            <img className="header__logo" alt="onTrack"></img>
-            <h1 className="header__title">onTrack</h1>
+            <img className="header__logo" src={logo} alt="onTrack"></img>
+            <div className="header__user">
+                {loggedIn ? <button>Log In</button> : <h1 className="header__title">Welcome, User</h1>}
+                <i className="bi-person-circle header__profile"></i>
+            </div>
         </header>
     );
 }
