26a8c779b05f3ea0aa06a7b7b7a0f978d203c311
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-19 16:57:59 -0400

    Add app description to landing

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -18,7 +18,11 @@ const hero = () => {
     return (
         <Parallax className="landing" blur={3} bgImage={landingImage} strength={600}>
             <div className="hero__content">
-                <h1 className="hero__title">Get Started</h1>
+                <h1 className="hero__title">onTrack</h1>
+                <span className="hero__description">Bring meaning to your workouts by using onTrack to plan out and reach your exercise goals.
+                    Record sets, reps, weight to start to visualizing your progress in a new way and identify areas of improvement.
+                </span>
+                <h2 className="hero__subtitle">Get Started</h2>
                 <button className="hero__cta">Sign Up</button>
                 <div className="hero__secondary-cta">
                     <span className="hero__login-prompt">Already have an account?</span>
