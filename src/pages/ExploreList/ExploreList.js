dc1138ad3e0e34ece50fe8e98ffb18b9e80295ba
sapphira-zheng <sapphirazheng@gmail.com>
2022-09-20 01:03:07 -0400

    Set up boilerplate for explore page

---
+++ src/pages/ExploreList/ExploreList.js
@@ -0,0 +1,13 @@
+import './ExploreList.scss';
+
+const ExploreList = () => {
+
+
+    return (
+        <section className="explore-list">
+            <h1 className="explore-list__title">Explore Exercises</h1>
+        </section>
+    );
+}
+
+export default ExploreList;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,8 @@
 import './HomePage.scss';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 
-const HomePage = ({  }) => {
-    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-    
+const HomePage = () => {
+     //will be progress page
+     //find way to highlight current tab
 
     return (
         <></>
